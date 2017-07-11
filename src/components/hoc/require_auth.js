68d7ff24059a1dc7ebe45b001ce7fde1cfa9df8b
Lucas Grimauth <lgevan@br.ibm.com>
2017-07-11 16:36:29 -0300

    Higher Order Components complete

---
+++ src/components/hoc/require_auth.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+		static contextTypes = {
+			router: React.PropTypes.object
+		}
+		
+		componentWillMount() {
+			if(!this.props.isLoggedIn){
+				this.context.router.push('/');
+			}
+		}
+
+		componentWillUpdate(nextProps) {
+			if(!nextProps.isLoggedIn){
+				this.context.router.push('/');
+			}
+		}
+
+		render(){
+			return <ComposedComponent {...this.props} />
+		}
+	}
+
+	function mapStateToProps(state) {
+		return { isLoggedIn: state.authenticated }
+	}
+
+	return connect(mapStateToProps)(Authentication);
+}
